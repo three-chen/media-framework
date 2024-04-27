d0531fd38adef477cc9f104b614c322e0c7cbce1
anember <anember@163.com>
2024-04-28 02:27:13 +0800

    rtmp推流端构建成功

--- src/factory/MediaFactory.ts
+++ src/factory/MediaFactory.ts
@@ -1,152 +1,291 @@
-import { FFmpeg } from '@ffmpeg/ffmpeg'
-import { toBlobURL } from '@ffmpeg/util'
-import { ffmpegBaseUrl } from '../constants'
-import type { LogEventCallback } from './types'
-// const os = require('os')
+const { ipcRenderer } = require('electron')
+
+import type { ExecCallback } from './types'
 
 export class MediaFactory {
-  private static ffmpeg = new FFmpeg()
+  public static plat: string = process.platform
 
-  //   public static plat = os.platform()
+  public static async init() {}
 
-  public static isLoadFfmpegCore = false
+  public static async checkInit() {}
 
-  public static async init() {
-    if (!MediaFactory.isLoadFfmpegCore) {
-      await MediaFactory.ffmpeg.load({
-        coreURL: await toBlobURL(`${ffmpegBaseUrl}/ffmpeg-core.js`, 'text/javascript'),
-        wasmURL: await toBlobURL(`${ffmpegBaseUrl}/ffmpeg-core.wasm`, 'application/wasm'),
-        workerURL: await toBlobURL(`${ffmpegBaseUrl}/ffmpeg-core.worker.js`, 'text/javascript')
-      })
-      MediaFactory.isLoadFfmpegCore = true
-    }
-  }
-
-  public static async checkInit() {
-    if (!MediaFactory.isLoadFfmpegCore) {
-      await MediaFactory.init()
-    }
-  }
-
-  public static async run(command: string[]) {
+  public static async runWithExec(command: string, cb?: ExecCallback) {
     await MediaFactory.checkInit()
-    MediaFactory.ffmpeg.on('log', logEvent => {
-      console.log(logEvent)
-    })
-    // 执行FFmpeg命令
-    const result = await MediaFactory.ffmpeg.exec(command)
-
-    if (result === 0) {
-      console.log('FFmpeg命令执行成功')
-    }
-
-    return result
+    console.log('runWithExec', command)
+    ipcRenderer.send('ffmpegCommandExec', command)
   }
 
-  public static async runWithLog(command: string[], cb: LogEventCallback) {
+  public static async runWithSpawn(command: string, args: string[], cb?: ExecCallback) {
     await MediaFactory.checkInit()
-    MediaFactory.ffmpeg.on('log', cb)
-    // 执行FFmpeg命令
-    const result = await MediaFactory.ffmpeg.exec(command)
 
-    if (result === 0) {
-      console.log('FFmpeg命令执行成功')
+    ipcRenderer.send('ffmpegCommandSpawn', JSON.stringify({ command, args }))
+    // const ChildProcess = spawn(command, args)
+    // console.log('ChildProcess', ChildProcess)
+  }
+
+  public static async listDevices() {
+    if (MediaFactory.plat == 'linux') {
+      MediaFactory.runWithExec('ffmpeg -v info -f avfoundation -list_devices true -i')
+    } else if (MediaFactory.plat == 'win32') {
+      MediaFactory.runWithExec('ffmpeg -list_devices true -f dshow -i dummy')
     }
-    MediaFactory.ffmpeg.off('log', cb)
   }
 
-  public static async listDevices(cb: LogEventCallback) {
-    // if (MediaFactory.plat == 'darwin') {
-    //   MediaFactory.run(['-v', 'info', '-f', 'avfoundation', '-list_devices', 'true', '-i'])
-    // } else if (MediaFactory.plat == 'win32') {
-    // MediaFactory.runWithLog(['-list_devices', 'true', '-f', 'dshow', '-i', 'dummy'], cb)
-    MediaFactory.runWithLog(['ffmpeg', '-v', 'info', '-f', 'avfoundation', '-list_devices', 'true', '-i', 'dummy'], cb)
-    // }
+  public static async cameraStreamExec(cb?: ExecCallback) {
+    if (MediaFactory.plat == 'linux') {
+      MediaFactory.runWithExec(
+        'ffmpeg -f v4l2 -i /dev/video0 -f alsa -ac 2 -i hw:1,0 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -r 30 -s 640x480 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://localhost/live/livestream',
+        cb
+          ? cb
+          : (error: ExecException | null, stdout: string, stderr: string) => {
+              console.log('error', error)
+              console.log('stdout', stdout)
+              console.log('stderr', stderr)
+            }
+      )
+    } else if (MediaFactory.plat == 'win32') {
+      MediaFactory.runWithExec(
+        'ffmpeg -f dshow -i video="HP Wide Vision HD Camera":audio="麦克风阵列 (英特尔® 智音技术)" -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -r 30 -s 640x480 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://localhost/live/livestream',
+        cb
+          ? cb
+          : (error: ExecException | null, stdout: string, stderr: string) => {
+              console.log('error', error)
+              console.log('stdout', stdout)
+              console.log('stderr', stderr)
+            }
+      )
+    }
+  }
+  public static async desktopStreamExec(cb?: ExecCallback) {
+    if (MediaFactory.plat == 'linux') {
+      MediaFactory.runWithExec(
+        'ffmpeg -f x11grab -s 1920x1080 -i :0.0 -f alsa -ac 2 -i hw:1,0 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -r 30 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://localhost/live/livestream',
+        cb
+          ? cb
+          : (error: ExecException | null, stdout: string, stderr: string) => {
+              console.log('error', error)
+              console.log('stdout', stdout)
+              console.log('stderr', stderr)
+            }
+      )
+    } else if (MediaFactory.plat == 'win32') {
+      MediaFactory.runWithExec(
+        'ffmpeg -f gdigrab -i desktop -f dshow -i audio="麦克风阵列 (英特尔® 智音技术)" -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -r 30 -s 1920x1080 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://localhost/live/livestream',
+        cb
+          ? cb
+          : (error: ExecException | null, stdout: string, stderr: string) => {
+              console.log('error', error)
+              console.log('stdout', stdout)
+              console.log('stderr', stderr)
+            }
+      )
+    }
   }
 
-  public static async cameraStream(cb?: LogEventCallback) {
-    // if (MediaFactory.plat == 'darwin') {
-    // } else if (MediaFactory.plat == 'win32') {
-    MediaFactory.run([
-      '-f',
-      'dshow',
-      '-i',
-      'video="HP Wide Vision HD Camera":audio="麦克风阵列 (英特尔® 智音技术)"',
-      '-c:v',
-      'libx264',
-      '-preset',
-      'veryfast',
-      '-maxrate',
-      '3000k',
-      '-bufsize',
-      '6000k',
-      '-pix_fmt',
-      'yuv420p',
-      '-g',
-      '50',
-      '-r',
-      '30',
-      '-s',
-      '1280x720',
-      '-c:a',
-      'aac',
-      '-b:a',
-      '160k',
-      '-ac',
-      '2',
-      '-ar',
-      '44100',
-      '-f',
-      'flv',
-      'rtmp://localhost/live/livestream'
-    ])
-    // }
+  public static async cameraStreamSpawn(cb?: ExecCallback) {
+    if (MediaFactory.plat == 'linux') {
+      MediaFactory.runWithSpawn(
+        'ffmpeg',
+        // ['-f v4l2 -i /dev/video0 -f alsa -ac 2 -i hw:1,0 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -r 30 -s 640x480 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://localhost/live/livestream'],
+        [
+          '-f',
+          'v4l2',
+          '-i',
+          '/dev/video0',
+          '-f',
+          'alsa',
+          '-ac',
+          '2',
+          '-i',
+          'hw:1,0',
+          '-c:v',
+          'libx264',
+          '-preset',
+          'veryfast',
+          '-maxrate',
+          '3000k',
+          '-bufsize',
+          '6000k',
+          '-pix_fmt',
+          'yuv420p',
+          '-g',
+          '50',
+          '-r',
+          '30',
+          '-s',
+          '640x480',
+          '-c:a',
+          'aac',
+          '-b:a',
+          '160k',
+          '-ac',
+          '2',
+          '-ar',
+          '44100',
+          '-f',
+          'flv',
+          'rtmp://localhost/live/livestream'
+        ],
+        cb
+          ? cb
+          : (error: ExecException | null, stdout: string, stderr: string) => {
+              console.log('error', error)
+              console.log('stdout', stdout)
+              console.log('stderr', stderr)
+            }
+      )
+    } else if (MediaFactory.plat == 'win32') {
+      MediaFactory.runWithSpawn(
+        'ffmpeg',
+        [
+          '-f',
+          'dshow',
+          '-i',
+          'video="HP Wide Vision HD Camera":audio="麦克风阵列 (英特尔® 智音技术)"',
+          '-c:v',
+          'libx264',
+          '-preset',
+          'veryfast',
+          '-maxrate',
+          '3000k',
+          '-bufsize',
+          '6000k',
+          '-pix_fmt',
+          'yuv420p',
+          '-g',
+          '50',
+          '-r',
+          '30',
+          '-s',
+          '640x480',
+          '-c:a',
+          'aac',
+          '-b:a',
+          '160k',
+          '-ac',
+          '2',
+          '-ar',
+          '44100',
+          '-f',
+          'flv',
+          'rtmp://localhost/live/livestream'
+        ],
+        // 'ffmpeg -f dshow -i video="HP Wide Vision HD Camera":audio="麦克风阵列 (英特尔® 智音技术)" -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -r 30 -s 640x480 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://localhost/live/livestream',
+        cb
+          ? cb
+          : (error: ExecException | null, stdout: string, stderr: string) => {
+              console.log('error', error)
+              console.log('stdout', stdout)
+              console.log('stderr', stderr)
+            }
+      )
+    }
   }
-  public static async desktopStream(cb?: LogEventCallback) {
-    // if (MediaFactory.plat == 'darwin') {
-    // } else if (MediaFactory.plat == 'win32') {
-    MediaFactory.run([
-      '-f',
-      'gdigrab',
-      '-i',
-      'desktop',
-      '-f',
-      'dshow',
-      '-i',
-      'audio="麦克风阵列 (英特尔® 智音技术)"',
-      '-c:v',
-      'libx264',
-      '-preset',
-      'veryfast',
-      '-maxrate',
-      '3000k',
-      '-bufsize',
-      '6000k',
-      '-pix_fmt',
-      'yuv420p',
-      '-g',
-      '50',
-      '-r',
-      '30',
-      '-s',
-      '1280x720',
-      '-c:a',
-      'aac',
-      '-b:a',
-      '160k',
-      '-ac',
-      '2',
-      '-ar',
-      '44100',
-      '-f',
-      'flv',
-      'rtmp://localhost/live/livestream'
-    ])
-    // }
+  public static async desktopStreamSpawn(cb?: ExecCallback) {
+    if (MediaFactory.plat == 'linux') {
+      MediaFactory.runWithSpawn(
+        'ffmpeg',
+        [
+          '-f',
+          'x11grab',
+          '-s',
+          '1920x1080',
+          '-i',
+          ':0.0',
+          '-f',
+          'alsa',
+          '-ac',
+          '2',
+          '-i',
+          'hw:1,0',
+          '-c:v',
+          'libx264',
+          '-preset',
+          'veryfast',
+          '-maxrate',
+          '3000k',
+          '-bufsize',
+          '6000k',
+          '-pix_fmt',
+          'yuv420p',
+          '-g',
+          '50',
+          '-r',
+          '30',
+          '-c:a',
+          'aac',
+          '-b:a',
+          '160k',
+          '-ac',
+          '2',
+          '-ar',
+          '44100',
+          '-f',
+          'flv',
+          'rtmp://localhost/live/livestream'
+        ],
+        // 'ffmpeg -f x11grab -s 1920x1080 -i :0.0 -f alsa -ac 2 -i hw:1,0 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -r 30 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://localhost/live/livestream',
+        cb
+          ? cb
+          : (error: ExecException | null, stdout: string, stderr: string) => {
+              console.log('error', error)
+              console.log('stdout', stdout)
+              console.log('stderr', stderr)
+            }
+      )
+    } else if (MediaFactory.plat == 'win32') {
+      MediaFactory.runWithSpawn(
+        'ffmpeg',
+        [
+          '-f',
+          'gdigrab',
+          '-i',
+          'desktop',
+          '-f',
+          'dshow',
+          '-i',
+          'audio="麦克风阵列 (英特尔® 智音技术)"',
+          '-c:v',
+          'libx264',
+          '-preset',
+          'veryfast',
+          '-maxrate',
+          '3000k',
+          '-bufsize',
+          '6000k',
+          '-pix_fmt',
+          'yuv420p',
+          '-g',
+          '50',
+          '-r',
+          '30',
+          '-s',
+          '1920x1080',
+          '-c:a',
+          'aac',
+          '-b:a',
+          '160k',
+          '-ac',
+          '2',
+          '-ar',
+          '44100',
+          '-f',
+          'flv',
+          'rtmp://localhost/live/livestream'
+        ],
+        // 'ffmpeg -f gdigrab -i desktop -f dshow -i audio="麦克风阵列 (英特尔® 智音技术)" -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 -r 30 -s 1920x1080 -c:a aac -b:a 160k -ac 2 -ar 44100 -f flv rtmp://localhost/live/livestream',
+        cb
+          ? cb
+          : (error: ExecException | null, stdout: string, stderr: string) => {
+              console.log('error', error)
+              console.log('stdout', stdout)
+              console.log('stderr', stderr)
+            }
+      )
+    }
   }
 
-  public static async test(command: string[]) {
-    await MediaFactory.checkInit()
+  public static async test(command: string) {
     console.log('test', command)
   }
 }
--- src/factory/types.ts
+++ src/factory/types.ts
@@ -1,6 +1,10 @@
+import type { ExecException } from 'child_process'
+
 export interface LogEvent {
   type: string
   message: string
 }
 
 export type LogEventCallback = (event: LogEvent) => void
+
+export type ExecCallback = (error: ExecException | null, stdout: string, stderr: string) => void
